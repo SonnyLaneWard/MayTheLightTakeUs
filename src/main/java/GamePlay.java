6ae1459f64042753f263f2df9c07080222fc327f
SonnyLaneWard <ivan.semejkin@yandex.ru>
2021-08-24 10:42:29 +0300

    BeginFilestest

--- src/main/java/GamePlay.java
+++ src/main/java/GamePlay.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
 public class GamePlay {
     public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choose language");
+        int choselanguage = in.nextInt();
+
+
+        Story begin = new Story();
+        begin.number(10);
 
     }
 }
--- src/main/java/Story.java
+++ src/main/java/Story.java
@@ -1,5 +1,44 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Story {
 
+public String number(int num_story)
+{
+    String s = null;
+    switch (num_story){
+        case (10) :
+            try(BufferedReader br = new BufferedReader(new FileReader("BeginEn.txt")))
+            {
+                while((s=br.readLine())!=null){
+                    s=br.readLine();
+
+                }
+            }
+            catch(IOException ex){
+
+                System.out.println(ex.getMessage());
+            }
+            break;
+
+        case (11) :
+            try(BufferedReader br = new BufferedReader(new FileReader("BeginRu.txt")))
+            {
+                while((s=br.readLine())!=null){
+                    s=br.readLine();
+
+                }
+            }
+            catch(IOException ex){
+
+                System.out.println(ex.getMessage());
+            }
+            break;
+    }
+
+    return s;
+}
 
 
 
