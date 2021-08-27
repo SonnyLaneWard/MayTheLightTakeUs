7b7c523aac13dcb3efc84ac374ad2ea4cc1fb4a1
SonnyLaneWard <ivan.semejkin@yandex.ru>
2021-08-27 14:27:12 +0300

    Storylichascii2

--- src/main/java/PrintLich.java
+++ src/main/java/PrintLich.java
@@ -3,19 +3,40 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class PrintLich {
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
 
     public static void print()
     {
         try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\sin\\IdeaProjects\\MayTheLightTakeUs\\src\\main\\java\\LichAscii.txt")))
         {
 
-            for (int i = 0;i < 20;i++)
+            for (int i = 0;i < 18;i++)
             {
                 String s = br.readLine();
                 System.out.println(s);
             }
 
 
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+    public static void printdead()
+    {
+        try(BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\sin\\IdeaProjects\\MayTheLightTakeUs\\src\\main\\java\\LichAscii.txt")))
+        {
+
+            for (int i = 0;i < 11;i++)
+            {
+                String s = br.readLine();
+                System.out.println(s);
+            }
+          System.out.println(RED + "DEAD DEAD DEAD DEAD DEAD");
+            System.out.println(RED + "DEAD DEAD DEAD DEAD DEAD" + RESET);
         }
         catch(IOException ex){
 
